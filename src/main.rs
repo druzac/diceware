5d9a505fa8e67744703433ceebb73528c29f291a
druzac <zachdrudi@gmail.com>
2021-01-20 03:10:34 -0500

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use getrandom;
 
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
+const DICEWARE: &str = include_str!("diceware8k.txt");
 
 fn get_random() -> Result<u16, getrandom::Error> {
     let mut buf: [u8; 2] = [0; 2];
@@ -10,31 +8,16 @@ fn get_random() -> Result<u16, getrandom::Error> {
     Ok((buf[0] as u16) + ((buf[1] as u16 & 31) << 8))
 }
 
-fn read_diceware<P: AsRef<std::path::Path>>(path: P) -> std::io::Result<Vec<String>> {
-    let file = File::open(path)?;
-    let result = BufReader::new(file).lines().collect::<std::io::Result<Vec<_>>>()?;
-    assert_eq!(result.len(), 8192);
-    Ok(result)
+fn dw_dictionary() -> Vec<&'static str> {
+    DICEWARE.split("\n").collect()
 }
 
-// should move this to a top level constant?
-fn get_embedded_diceware() -> &'static str {
-    include_str!("diceware8k.txt")
-}
-
-fn get_vec() -> Vec<&'static str> {
-    get_embedded_diceware().split("\n").collect()
-}
-
-// first arg: path to diceware file
-// second arg: number of words
+// first arg: number of words
 fn main() -> std::io::Result<()> {
-    // what do we want to get?
-    // we want 13 bits. so i need to get 2 bytes. and shift by 3 to the right.
     let mut args: Vec<std::ffi::OsString> = std::env::args_os().collect();
     assert!(args.len() >= 2);
 
-    let words = get_vec();
+    let words = dw_dictionary();
     let word_count: u64 = args.remove(1).into_string().unwrap().parse().unwrap();
     let mut pass_words: Vec<&str> = vec![];
     for _ in 0..word_count {
