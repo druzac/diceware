e8d9dc79aa948e2033f87c290e1645c57339edb8
druzac <zachdrudi@gmail.com>
2021-01-23 18:07:02 -0500

    proper error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use getrandom;
 
+use std::io;
+
 const DICEWARE: &str = include_str!("diceware8k.txt");
 
 fn get_random() -> Result<u16, getrandom::Error> {
@@ -12,17 +14,32 @@ fn dw_dictionary() -> Vec<&'static str> {
     DICEWARE.split("\n").collect()
 }
 
-// first arg: number of words
-fn main() -> std::io::Result<()> {
-    let mut args: Vec<std::ffi::OsString> = std::env::args_os().collect();
+fn parse_num_words(num: &std::ffi::OsStr) -> io::Result<u64> {
+    let s = num.to_str().ok_or(io::Error::new(
+        io::ErrorKind::Other,
+        "unparseable command line argument.",
+    ))?;
+    s.parse::<u64>().or_else(|e| {
+        Err(io::Error::new(
+            io::ErrorKind::Other,
+            format!("Couldn't parse number: {:?}", e),
+        ))
+    })
+}
+
+fn main() -> io::Result<()> {
+    let args: Vec<std::ffi::OsString> = std::env::args_os().collect();
+    if args.len() < 2 {
+        return Err(io::Error::new(io::ErrorKind::Other, "1 argument required"));
+    }
     assert!(args.len() >= 2);
 
     let words = dw_dictionary();
-    let word_count: u64 = args.remove(1).into_string().unwrap().parse().unwrap();
+    let word_count = parse_num_words(&args[1])?;
     let mut pass_words: Vec<&str> = vec![];
     for _ in 0..word_count {
-	let idx = get_random()?;
-	pass_words.push(words[idx as usize]);
+        let idx = get_random()?;
+        pass_words.push(words[idx as usize]);
     }
     println!("{}", pass_words.join(","));
     Ok(())
